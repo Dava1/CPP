88fd78ba7f9602c5177d1dd88a37f8c481245b5e
Dava1 <se_fi_rot@mail.ru>
2019-10-06 21:51:44 +0200

    commit two

---
+++ cvicka2/cvicka2/RandomNum.c
@@ -0,0 +1,25 @@
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int generateNumber(int min , int max)
+{
+	return rand() % (max - min) + min;
+}
+
+
+int main(int argc, const char* argv[]) {
+
+
+
+	for (size_t i = 0; i < 1000; i++)
+	{
+		printf("%d\n", generateNumber(-1000,1000));
+	}
+	system("pause");
+
+
+	return 0;
+}
+*/
